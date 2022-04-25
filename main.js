b23cd4211e6336ab5e790faaaf5dab7bea7bb894
Daniel B. Zhang <42197094+danielbzhang@users.noreply.github.com>
2022-04-25 15:59:10 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+// set constant kelvin
 const kelvin = 293;
+// set celsius
+const celsius = kelvin - 273;
+// calculate F
+let fahrenheit = celsius * (9/5) + 32;
+// round F
+fahrenheit = fahrenheit.floor();
+
+console.log(`The temperature is ${fahrenheit}.`);
